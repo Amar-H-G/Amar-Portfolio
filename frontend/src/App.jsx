7ee44462734f769c3b32e531e816b3d42cd1730b
Amar Patra <amarpatra932@gmail.com>
2025-07-17 18:03:59 +0530

    update resume

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import Footer from "./components/Home/Footer";
 import Experience from "./pages/Experience";
 import Skills from "./pages/Skills";
 import Work from "./pages/Work";
-import Services from "./pages/Services";
+import Services from "./pages/services";
 
 function App() {
   return (
